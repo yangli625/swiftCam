58072fb1317e69293fe5aa2318f0bb7adea00443
Yang Li <yangli625@gmail.com>
2015-05-02 12:02:04 -0400

    Initial Commit

---
+++ PhotoAge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoAge
+//
+//  Created by Yang LI on 5/2/15.
+//  Copyright (c) 2015 Yang LI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
